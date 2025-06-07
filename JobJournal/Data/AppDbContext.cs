b66f7ec9d6dab7ad839835be0575abafb9b2407f
Tirth <tirthacodes@gmail.com>
2025-06-07 10:11:40 +0545

    downgraded all the packages to 8.0.0 version to make all packages compatible

--- JobJournal/Data/AppDbContext.cs
+++ JobJournal/Data/AppDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace JobJournal.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : DbContext, IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
