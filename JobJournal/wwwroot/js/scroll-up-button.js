a056b95777156ffd448356a421cf89ee4a3de153
Tirth <tirthacodes@gmail.com>
2025-06-10 22:21:38 +0545

    added a arrow button to scroll to top when the contents are srolled below

---
+++ JobJournal/wwwroot/js/scroll-up-button.js
@@ -0,0 +1,13 @@
+﻿const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+});
+
+scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+});
