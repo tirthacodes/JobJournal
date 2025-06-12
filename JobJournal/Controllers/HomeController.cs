834347445bf1009cca213fe40acba8759e0f3316
Tirth <tirthacodes@gmail.com>
2025-06-12 23:53:43 +0545

    adding a view model to display data on index page and view for it

--- JobJournal/Controllers/HomeController.cs
+++ JobJournal/Controllers/HomeController.cs
@@ -1,24 +1,36 @@
+using JobJournal.Data.Enums;
+using JobJournal.Data;
 using JobJournal.Models;
+using JobJournal.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
+using Microsoft.EntityFrameworkCore;
 
 namespace JobJournal.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AppDbContext context, UserManager<IdentityUser> userManager)
         {
             _logger = logger;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            
+
             return View();
         }
 
-        
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ JobJournal/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace JobJournal.ViewModels
+{
+    public class DashboardViewModel
+    {
+        public int ApplicationsThisWeek { get; set; }
+        public int ApplicationsThisMonth { get; set; }
+        public int InterviewsScheduled { get; set; }
+        public int OffersReceived { get; set; }
+        public int FollowUpsDue { get; set; }
+        public int TotalApplications { get; set; }
+    }
+}
