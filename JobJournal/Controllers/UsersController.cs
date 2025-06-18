745750866813f9baa90b959ac8dc823e8b8dca97
Tirth <tirthacodes@gmail.com>
2025-06-18 17:54:30 +0545

    Displayed the user information

---
+++ JobJournal/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using JobJournal.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace JobJournal.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AppDbContext _context; 
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(AppDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+
+            if (currentUser == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            return View(currentUser);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
